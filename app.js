d648d822566f98040396b018ebe9801cc7a81c7f
plla2 <120915990+plla2@users.noreply.github.com>
2023-01-09 19:23:52 +0900

    DOM base

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
-const title = document.querySelector(".hello h1");
-title.innerText = "Click me!";
-console.log(title);
+const h1 = document.querySelector(".hello h1");
+h1.innerText = "Click me!";
+console.log(h1);
 
 function handleTitleClick() {
   console.log("title was clicked");
-  title.style.color = "blue";
+  h1.style.color = "blue";
 }
 
 function handleMouseEnter() {
-  title.innerText = "Mouse is here!";
+  h1.innerText = "Mouse is here!";
 }
 
 function handleMouseLeave() {
-  title.innerText = "Mouse is gone!";
+  h1.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick); // title을 클릭했을 때의 이벤트
-title.addEventListener("mouseenter", handleMouseEnter); // title위에 마우스를 올렸을 때의 이벤트
-title.addEventListener("mouseleave", handleMouseLeave); // title 위에서 마우스가 벗어났을 때의 이벤트
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("offline!");
+}
+function handleWindowOnline() {
+  alert("online!");
+}
+
+h1.addEventListener("click", handleTitleClick); // title을 클릭했을 때의 이벤트
+h1.addEventListener("mouseenter", handleMouseEnter); // title위에 마우스를 올렸을 때의 이벤트
+h1.addEventListener("mouseleave", handleMouseLeave); // title 위에서 마우스가 벗어났을 때의 이벤트
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
